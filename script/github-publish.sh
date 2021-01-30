a6255bfa9edf0fa839620764271250890a18df86
mikulas <mikulas.bankovic27@gmail.com>
2021-01-30 15:25:58 +0100

    Added test to main.yml and pull_request.yml. Added suffix to shell scripts to indicate file type Added github-test.sh that downloads, upscale video and compare sizes of final videos.

--- script/github-publish.sh
+++ script/github-publish.sh
@@ -7,6 +7,6 @@ set -o xtrace
 
 source ./environment/vars
 
-docker tag miratmu/ffmpeg-tensorflow:${DOCKER_TAG} miratmu/ffmpeg-tensorflow:latest
-docker push miratmu/ffmpeg-tensorflow:${DOCKER_TAG}
+docker tag miratmu/ffmpeg-tensorflow:"${DOCKER_TAG}" miratmu/ffmpeg-tensorflow:latest
+docker push miratmu/ffmpeg-tensorflow:"${DOCKER_TAG}"
 docker push miratmu/ffmpeg-tensorflow:latest
